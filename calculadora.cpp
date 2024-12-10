7212bed8a3dacadcb16e2d15f37bc77d2700ea29
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2024-12-09 19:12:42 -0700

    Initial commit

---
+++ calculadora.cpp
@@ -0,0 +1,25 @@
+#include "calculadora.hpp"
+
+// Suma dos numeros -----------------
+double suma(double x, double y)
+{
+    return 0;
+}
+
+// Resta dos numeros -----------------
+double resta(double x, double y)
+{
+    return 0;
+}
+
+// Multiplica dos numeros -------------
+double multiplica(double x, double y)
+{
+    return 0;
+}
+
+// Divide dos numeros -----------------
+double divide(double x, double y)
+{
+    return 0;
+}
